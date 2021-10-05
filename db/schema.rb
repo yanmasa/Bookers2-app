e2c3524cf43a27a838b9ae504f1f80a361152a18
yanmasa <ueda3ban@gmail.com>
2021-10-05 03:17:38 +0000

    dmvisual

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_021937) do
+ActiveRecord::Schema.define(version: 2021_10_05_015605) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
